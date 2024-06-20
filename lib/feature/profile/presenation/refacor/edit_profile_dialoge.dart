9f92f95c35a64618ee4feb12e69952b02dcf37e9
Shimaa-Saber <shimaasaber812@gmail.com>
2024-06-20 13:51:34 +0300

    hadle edit profile screen

--- lib/feature/profile/presenation/refacor/edit_profile_dialoge.dart
+++ lib/feature/profile/presenation/refacor/edit_profile_dialoge.dart
@@ -14,100 +14,101 @@ class EditProfileDialge extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        width: context.width*0.9,
-        height: context.height*0.9,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Stack(
-                children: [
-                  Container(
-                    width: context.width*0.39,
-                    height: context.height*0.39,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 4,color: Colors.blue),
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                          image: AssetImage('assets/images/patientfall.png'))
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      child: SingleChildScrollView(
+        child: Container(
+          width: context.width * 0.9,
+          height: context.height * 0.9,
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: context.width * 0.39,
+                      height: context.height * 0.39,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 4, color: Colors.blue),
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/images/patientfall.png'),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: context.height * 0.24,
+                      left: context.width * 0.26,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: context.width * 0.1,
+                          height: context.height * 0.034,
+                          color: Color(0xff00A9FF),
+                          child: Image.asset('assets/images/🦆 icon _camera_.png'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              verticalSpace(5),
+              editProfileColumn(
+                text: 'Deniz Can',
+                icon: Icons.perm_identity,
+              ),
+              verticalSpace(20),
+              editProfileColumn(
+                text: '01121010554',
+                icon: Icons.phone,
+              ),
+              verticalSpace(20),
+              editProfileColumn(
+                text: 'Female',
+                icon: Icons.female_outlined,
+              ),
+              verticalSpace(70),
+              Center(
+                child: Container(
+                  width: context.width * 0.5,
+                  height: context.height * 0.07,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color(0xff00A9FF),
+                    boxShadow: [
+                      BoxShadow(
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        color: Colors.black,
+                        blurStyle: BlurStyle.normal,
+                        offset: Offset(1, 1),
+                      ),
+                    ],
+                  ),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Edit',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
                   ),
-                  Positioned(
-                     top: context.height*0.24,
-                     left: context.width*0.26,
-                      child: GestureDetector(
-                        onTap: (){},
-                        child: Container(
-                          width: context.width*0.1,
-                            height: context.height*0.034,
-                            color: Color(0xff00A9FF),
-
-                            child: Image(image: AssetImage('assets/images/🦆 icon _camera_.png'),)),
-                      )
-                  )
-                    ,
-                ],
-
-              ),
-            ),
-           verticalSpace(5),
-            editProfileColumn(
-              text: 'Deniz Can',
-              icon: Icons.perm_identity,
-            ),
-            verticalSpace(20),
-
-            editProfileColumn(
-              text: '01121010554',
-              icon: Icons.phone,
-            ),
-
-            verticalSpace(20),
-
-            editProfileColumn(
-              text: 'Femal',
-              icon: Icons.female_outlined,
-            ),
-
-            verticalSpace(70),
-
-            Center(
-              child: Container(
-                width: context.width*0.5,
-                height: context.height*0.07,
-
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Color(0xff00A9FF) ,
-                  boxShadow: [
-                    BoxShadow(
-                      spreadRadius: 1,
-                      blurRadius: 5,
-                      color: Colors.black,
-                      blurStyle: BlurStyle.normal,
-                      offset: Offset.fromDirection(1)
-
-                    )
-                  ]
                 ),
-
-
-                child: Align(
-                  alignment: Alignment.center,
-
-                    child: Text('Edit',style: TextStyle(color: Colors.white,fontSize: 20.sp,fontWeight: FontWeight.w400),)),
               ),
-            )
-
-
-
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
