1f0a43e7d19004c126f32b2f016ba799c69a51cb
shaima-abdelbaset <shaimaaabdelbasset1@gmail.com>
2024-06-21 15:47:04 +0300

    chattscreen

---
+++ lib/feature/ChatScreen/presentation/widget/chattingbuble.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:fall_detection/core/styles/colors/colors.dart';
+
+class ChatBuble extends StatelessWidget {
+  const ChatBuble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        padding: EdgeInsets.only(left: 16, top: 32, bottom: 32, right: 16), // Ensure all padding values are provided
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(32),
+            topRight: Radius.circular(32),
+            bottomLeft: Radius.circular(32),
+          ),
+          color: AppColors.primaryColor,
+        ),
+        child: Text(
+          "Please, I need help here",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
