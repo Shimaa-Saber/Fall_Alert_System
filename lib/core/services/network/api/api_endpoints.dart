02e0cd8b34bcb0539436c1ab8d2e158423c219ca
shaima-abdelbaset <shaimaaabdelbasset1@gmail.com>
2024-06-29 20:30:45 +0300

    notificatins

--- lib/core/services/network/api/api_endpoints.dart
+++ lib/core/services/network/api/api_endpoints.dart
@@ -13,11 +13,14 @@ class EndPoints {
   static String getCountry = 'location/countries';
   static String getStates = 'location/states';
   static String getPatientProfile='patients/11';
+  static String getAllNotifications='notifications';
 
   static String homeScreen = "falls?deep=true";
   static String verifyUserAccount = 'caregivers/verify-email';
   static String resendCode = "caregivers/resend-code";
   static String resetPassword = "caregivers/reset-password";
+  static String logout='auth/logout';
+  static String chats='me/chats';
   //static String forgetPassword="caregivers/forgot-password";
 
   //caregivers/resend-code
---
+++ lib/feature/notification/presentation/manger/cubits/notificationsStates.dart
@@ -0,0 +1,21 @@
+import '../../../Data/Moddels/NotificationModel.dart';
+
+abstract class Notificationsstates {
+  const Notificationsstates();
+}
+
+class NotificationIntialState extends Notificationsstates {}
+
+class NotificationsLoading extends Notificationsstates {}
+
+class NotificationsLoadedSuccess extends Notificationsstates {
+  final NotificationsModel notifications; // Corrected line
+
+  NotificationsLoadedSuccess(this.notifications);
+}
+
+class NotificationsFailer extends Notificationsstates {
+  final String message;
+
+  const NotificationsFailer({required this.message});
+}
